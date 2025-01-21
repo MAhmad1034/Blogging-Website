726feab61fc9cff182c81d59947adbf940fb9ccf
l1f21bsse0597 <l1f21bsse0597@ucp.edu.pk>
2025-01-22 02:18:12 +0500

    initial commit

---
+++ config.sh
@@ -0,0 +1,6 @@
+echo "Configure Basic Git commands"
+git config --local user.name "l1f21bsse0597"
+git config --local user.email "l1f21bsse0597@ucp.edu.pk"
+git config --local core.editor notepad
+touch ./.gitignore
+
